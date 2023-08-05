c327d3154fec525d9c2cba3d298a59b35ca78fd7
mohamed abdelhedi <mohamed.abdelhedi@supcom.tn>
2023-08-05 15:04:47 +0100

    adding the side-bar

---
+++ FrontEND/src/app/dashboard-page/dashboard-page.component.ts
@@ -0,0 +1,32 @@
+import { Component } from '@angular/core';
+import {
+  faAddressCard,
+  faBookReader,
+  faDashboard, faGear,
+  faLaptop,
+  faLock,
+  faUser,
+  faUsers
+} from "@fortawesome/free-solid-svg-icons";
+
+@Component({
+  selector: 'dashboard-page',
+  templateUrl: './dashboard-page.component.html',
+  styleUrls: ['./dashboard-page.component.css']
+})
+export class DashboardPageComponent {
+
+
+  activeNavItem: number = 0;
+  onNavItemClicked(navItem: number) {
+    this.activeNavItem = navItem;
+  }
+
+  protected readonly faDashboard = faDashboard;
+  protected readonly faBookReader = faBookReader;
+  protected readonly faUser = faUser;
+  protected readonly faUsers = faUsers;
+  protected readonly faLaptop = faLaptop;
+  protected readonly faAddressCard = faAddressCard;
+  protected readonly faGear = faGear;
+}
