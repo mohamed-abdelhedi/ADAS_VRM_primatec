8badfa99f0d2c8ddf1a752ead9114763a60e163b
mohamed abdelhedi <mohamed.abdelhedi@supcom.tn>
2023-08-11 12:33:58 +0100

    edit "add employee form"

--- FrontEND/src/app/add-employee-next/add-employee-next.component.ts
+++ FrontEND/src/app/add-employee-next/add-employee-next.component.ts
@@ -1,6 +1,7 @@
 import { Component } from '@angular/core';
-import {faCamera} from "@fortawesome/free-solid-svg-icons";
-import {FormControl} from "@angular/forms";
+import {faAdd, faCamera, faDeleteLeft, faPlus, faTrash} from "@fortawesome/free-solid-svg-icons";
+import {FormArray, FormBuilder, FormControl, FormGroup} from "@angular/forms";
+import {MatSelectChange} from "@angular/material/select";
 
 @Component({
   selector: 'app-add-employee-next',
@@ -10,8 +11,7 @@ import {FormControl} from "@angular/forms";
 export class AddEmployeeNextComponent {
 
   protected readonly faCamera = faCamera;
-  toppings = new FormControl('');
-  toppingList: string[] = ['HTML', 'CSS', 'JavaScript', 'Angular', 'SpringBOOT', 'MySQL'];
+
   text: string = ''; // Holds the textarea input
   wordCount: number = 0; // Holds the word count
 
@@ -19,4 +19,37 @@ export class AddEmployeeNextComponent {
     const words = this.text.split(/\s+/).filter(Boolean);
     this.wordCount = words.length;
   }
+
+
+  diplomaForm: FormGroup;
+
+  constructor(private fb: FormBuilder) {
+    this.diplomaForm = this.fb.group({
+      diplomaGroups: this.fb.array([this.createDiplomaGroup()])
+    });
+  }
+
+  get diplomaGroups() {
+    return this.diplomaForm.get('diplomaGroups') as FormArray;
+  }
+
+  createDiplomaGroup() {
+    return this.fb.group({
+      diploma: '',
+      birthdate: ''
+    });
+  }
+
+  addDiplomaGroup() {
+    this.diplomaGroups.push(this.createDiplomaGroup());
+  }
+
+  deleteDiplomaGroup(index: number) {
+    this.diplomaGroups.removeAt(index);
+  }
+
+  protected readonly faDeleteLeft = faDeleteLeft;
+  protected readonly faTrash = faTrash;
+  protected readonly faAdd = faAdd;
+  protected readonly faPlus = faPlus;
 }
